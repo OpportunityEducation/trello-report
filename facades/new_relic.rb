4a41304e350e8ac178632675c1a215fdc5530e85
Scott Firestone <scott.firestone.dev@gmail.com>
2017-09-14 10:51:19 -0400

    Fix some calls

--- facades/new_relic.rb
+++ facades/new_relic.rb
@@ -7,7 +7,8 @@ class NewRelic
               :requests_per_minute,
               :average_response_time,
               :request_satisfaction,
-              :error_rate
+              :error_rate,
+              :time_frame
 
   def initialize
     @_api_key ||= ENV['NEW_RELIC_API_KEY']
@@ -16,7 +17,7 @@ class NewRelic
 
   def uptime(months_ago)
     response = RestClient.
-      get("#{api_url}/metrics/data.json?names[]=Errors/all&values[]=error_count&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
+      get("#{api_url}names[]=Errors/all&values[]=error_count&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
 
     response.body
   end
@@ -29,44 +30,34 @@ class NewRelic
 
   def requests_per_minute(months_ago)
     response = RestClient
-      .get("#{api_url}/metrics/data.json?names[]=Agent/MetricsReported/count&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
+      .get("#{api_url}names[]=Agent/MetricsReported/count&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
 
     parse(response)[:metric_data][:metrics][0][:timeslices][0][:values][:requests_per_minute]
   end
 
-  def error_rate(months_ago)
+  def error_count(months_ago)
     response = RestClient.
-      get("#{api_url}/metrics/data.json?names[]=Errors/all&values[]=errors_per_minute&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
+      get("#{api_url}names[]=Errors/all&values[]=errors_per_minute&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
 
-    error_rate = parse(response)[:metric_data][:metrics][0][:timeslices][0][:values][:errors_per_minute]
-    (error_rate * 100).round(2)
+    parse(response)[:metric_data][:metrics][0][:timeslices][0][:values][:errors_per_minute]
+  end
+
+  def error_rate(months_ago)
+    '%.2f' % ((100 * error_count(months_ago)) / total_requests(months_ago))
   end
 
   def average_response_time(months_ago)
     response = RestClient.
-      get("#{api_url}/metrics/data.json?names[]=HttpDispatcher&values[]=average_call_time&values[]=call_count&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
+      get("#{api_url}names[]=HttpDispatcher&values[]=average_response_time&values[]=call_count&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
 
-    parse(response)[:metric_data][:metrics][0][:timeslices][0][:values][:average_call_time]
+    parse(response)[:metric_data][:metrics][0][:timeslices][0][:values][:average_response_time]
   end
 
   def request_satisfaction(months_ago)
     response = RestClient.
-      get("#{api_url}/metrics/data.json?names[]=Apdex&names[]=EndUser/Apdex&values[]=score&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
+      get("#{api_url}names[]=Apdex&names[]=EndUser/Apdex&values[]=score&#{time_frame(months_ago)}&summarize=true", headers={ "x-api-key": api_key })
 
-    parse(response)[:metric_data][:metrics][0][:timeslices][0][:values][:score]
-  end
-
-  private
-  def api_key
-    @_api_key
-  end
-
-  def app_id
-    @_app_id
-  end
-
-  def api_url
-    "https://api.newrelic.com/v2/applications/" + app_id
+    parse(response)[:metric_data][:metrics][0][:timeslices][0][:values][:score] * 100
   end
 
   def time_frame(months_ago)
@@ -81,7 +72,7 @@ class NewRelic
         start_date = date.prev_month.beginning_of_month
         end_date = date.prev_month.end_of_month
       when 0
-        start_date = date.prev_month.beginning_of_month
+        start_date = date.beginning_of_month
         end_date = date
     end
 
@@ -91,6 +82,19 @@ class NewRelic
     "from=#{start_date}to=#{end_date}"
   end
 
+  private
+  def api_key
+    @_api_key
+  end
+
+  def app_id
+    @_app_id
+  end
+
+  def api_url
+    "https://api.newrelic.com/v2/applications/" + app_id + "/metrics/data.json?"
+  end
+
   def minutes_in_the_month(months_ago)
     case months_ago
       when 2
@@ -98,7 +102,7 @@ class NewRelic
       when 1
         Time.days_in_month(Time.now.prev_month.month, Time.now.prev_month.year) * 24 * 60
       when 0
-        Time.days_in_month(Time.now.month, Time.now.year) * 24 * 60
+        ((Time.now - Time.now.beginning_of_month) / 60)
     end
   end
 
