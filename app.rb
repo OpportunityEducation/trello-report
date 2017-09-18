622ef3d81241f5eac910258973adc15ca75c37e2
Scott Firestone <scott.firestone.dev@gmail.com>
2017-09-18 16:48:12 -0600

    More updates

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ get '/' do
 end
 
 get '/new-relic' do
-  # cache_control :public, max_age: 1800
+  cache_control :public, max_age: 1800
 
   months = params[:months] || 3
   @summaries = (0..(months.to_i - 1)).collect{ |i| m = DateTime.now << i; NewRelic.new(OpenStruct.new(year: m.year, month: m.month)) }
--- facades/new_relic.rb
+++ facades/new_relic.rb
@@ -2,17 +2,13 @@ require "rest-client"
 require "active_support/core_ext"
 
 class NewRelic
-  attr_reader :start_time, :end_time, :minutes_in_the_month, :month, :year, :time_frame
+  attr_reader :month, :year
 #
   def initialize(args)
     @_api_key ||= ENV["NEW_RELIC_API_KEY"]
     @_app_id ||= ENV["NEW_RELIC_APP_ID"]
     @month = args.month
     @year = args.year
-    @start_time = get_start_time
-    @end_time = get_end_time
-    @time_frame = get_formatted_time_frame
-    @minutes_in_the_month = in_current_month ? ((Time.now - Time.now.beginning_of_month) / 60) : Time.days_in_month(args.month, args.year)
   end
 
   def uptime
@@ -20,7 +16,10 @@ class NewRelic
   end
 
   def total_requests
-    requests_per_minute * minutes_in_the_month
+    url = "#{api_url}names[]=HttpDispatcher&#{time_frame}&summarize=true"
+    response = get(url)
+
+    @total_requests ||= parse(response).metric_data.metrics[0].timeslices[0].values.call_count
   end
 
   def total_requests_formatted
@@ -28,10 +27,10 @@ class NewRelic
   end
 
   def requests_per_minute
-    url = "#{api_url}names[]=Agent/MetricsReported/count&#{time_frame}&summarize=true"
+    url = "#{api_url}names[]=HttpDispatcher&#{time_frame}&summarize=true"
     response = get(url)
 
-    parse(response).metric_data.metrics[0].timeslices[0].values.requests_per_minute
+    @requests_per_minute ||= parse(response).metric_data.metrics[0].timeslices[0].values.requests_per_minute
   end
 
   def error_count
@@ -42,7 +41,7 @@ class NewRelic
   end
 
   def error_rate
-    "%.2f" % ((100 * error_count) / total_requests)
+    "%.2f" % (100 * error_count / (total_requests + other_transactions_count))
   end
 
   def average_response_time
@@ -57,13 +56,6 @@ class NewRelic
     response = get(url)
 
     parse(response).metric_data.metrics[0].timeslices[0].values.score * 100
-  end
-
-  def get_formatted_time_frame
-    formatted_start_time = start_time.iso8601.gsub("Z", '+00:00')
-    formatted_end_time = end_time.iso8601.gsub("Z", '+00:00')
-
-    "from=#{formatted_start_time}&to=#{formatted_end_time}"
   end
 
   private
@@ -87,24 +79,41 @@ class NewRelic
     JSON.parse(response.body, object_class: OpenStruct)
   end
 
-
-  def get_minutes_in_the_month
-    in_current_month ? ((Time.now - Time.now.beginning_of_month) / 60) : (Time.days_in_month(month, year) * 24 * 60)
+  def start_time
+    @start_time ||= Time.new(year, month).utc.at_beginning_of_month
   end
 
-  def get_start_time
-    Time.new(year, month).at_beginning_of_month.utc
-  end
+  def end_time
+    end_time = ''
 
-  def get_end_time
     if in_current_month
-      Time.now.utc
+      end_time = Time.now.utc
     else
-      Time.new(year, month).at_end_of_month.utc
+      end_time = Time.new(year, month).utc.at_end_of_month
     end
+
+    @end_time ||= end_time
   end
 
   def in_current_month
     Time.now.month == month && Time.now.year == year
+  end
+
+  def time_frame
+    formatted_start_time = start_time.iso8601.gsub("Z", '+00:00')
+    formatted_end_time = end_time.iso8601.gsub("Z", '+00:00')
+
+    @time_frame ||= "from=#{formatted_start_time}&to=#{formatted_end_time}"
+  end
+
+  def minutes_in_the_month
+    @minutes_in_month ||= in_current_month ? (Time.now - Time.now.beginning_of_month) / 60 : (Time.days_in_month(month, year) * 24 * 60)
+  end
+
+  def other_transactions_count
+    url = "#{api_url}names[]=OtherTransaction/all&#{time_frame}&summarize=true"
+    response = get(url)
+
+    parse(response).metric_data.metrics[0].timeslices[0].values.call_count
   end
 end
