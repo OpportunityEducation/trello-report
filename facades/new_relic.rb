85d20b5035d9e6d1e7a437e606f944e557f76805
Scott Firestone <scott.firestone.dev@gmail.com>
2017-09-22 11:14:44 -0600

    Refactor

--- facades/new_relic.rb
+++ facades/new_relic.rb
@@ -5,8 +5,6 @@ class NewRelic
   attr_reader :month, :year
 #
   def initialize(year, month)
-    @_api_key ||= ENV["NEW_RELIC_API_KEY"]
-    @_app_id ||= ENV["NEW_RELIC_APP_ID"]
     @year = year
     @month = month
   end
@@ -23,7 +21,7 @@ class NewRelic
     url = "#{api_url}names[]=HttpDispatcher&#{time_frame}&summarize=true"
     response = get(url)
 
-    @requests_per_minute ||= parse(response).metric_data.metrics[0].timeslices[0].values.requests_per_minute
+    @requests_per_minute ||= response.metric_data.metrics[0].timeslices[0].values.requests_per_minute
   end
 
   def error_rate
@@ -34,39 +32,35 @@ class NewRelic
     url = "#{api_url}names[]=HttpDispatcher&values[]=average_response_time&#{time_frame}&summarize=true"
     response = get(url)
 
-    parse(response).metric_data.metrics[0].timeslices[0].values.average_response_time
+    response.metric_data.metrics[0].timeslices[0].values.average_response_time
   end
 
   def request_satisfaction
     url = "#{api_url}names[]=Apdex&names[]=EndUser/Apdex&values[]=score&#{time_frame}&summarize=true"
     response = get(url)
 
-    parse(response).metric_data.metrics[0].timeslices[0].values.score * 100
+    response.metric_data.metrics[0].timeslices[0].values.score * 100
   end
 
   private
   def api_key
-    @_api_key
+    ENV["NEW_RELIC_API_KEY"]
   end
 
   def app_id
-    @_app_id
+    ENV["NEW_RELIC_APP_ID"]
   end
 
   def api_url
-    "https://api.newrelic.com/v2/applications/" + app_id + "/metrics/data.json?"
+    "https://api.newrelic.com/v2/applications/#{app_id}/metrics/data.json?"
   end
 
   def get(url)
-    RestClient.get(url, headers={"x-api-key": api_key})
-  end
-
-  def parse(response)
-    JSON.parse(response.body, object_class: OpenStruct)
+    JSON.parse(RestClient.get(url, headers={"x-api-key": api_key}).body, object_class: OpenStruct)
   end
 
   def start_time
-    @start_time ||= Time.new(year, month).utc.at_beginning_of_month
+    Time.new(year, month).utc.at_beginning_of_month
   end
 
   def end_time
@@ -78,34 +72,35 @@ class NewRelic
       end_time = Time.new(year, month).utc.at_end_of_month
     end
 
-    @end_time ||= end_time
+    end_time
   end
 
   def time_frame
     formatted_start_time = start_time.iso8601.gsub("Z", '+00:00')
     formatted_end_time = end_time.iso8601.gsub("Z", '+00:00')
 
-    @time_frame ||= "from=#{formatted_start_time}&to=#{formatted_end_time}"
+    "from=#{formatted_start_time}&to=#{formatted_end_time}"
   end
 
   def other_transactions_count
     url = "#{api_url}names[]=OtherTransaction/all&#{time_frame}&summarize=true"
     response = get(url)
 
-    parse(response).metric_data.metrics[0].timeslices[0].values.call_count
+    response.metric_data.metrics[0].timeslices[0].values.call_count
   end
 
   def error_count
     url ="#{api_url}names[]=Errors/all&#{time_frame}&summarize=true"
     response = get(url)
 
-    parse(response).metric_data.metrics[0].timeslices[0].values.error_count
+    response.metric_data.metrics[0].timeslices[0].values.error_count
   end
 
   def total_requests
-    url = "#{api_url}names[]=HttpDispatcher&#{time_frame}&summarize=true"
-    response = get(url)
-
-    @total_requests ||= parse(response).metric_data.metrics[0].timeslices[0].values.call_count
+    @total_requests ||= begin
+      url = "#{api_url}names[]=HttpDispatcher&#{time_frame}&summarize=true"
+      response = get(url)
+      response.metric_data.metrics[0].timeslices[0].values.call_count
+    end
   end
 end
